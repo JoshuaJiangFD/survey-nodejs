a420a3cda7298e834c15a0949f30a51a8c247dd3
JoshuaJiangFD <776456006@qq.com>
2015-06-17 20:57:03 +0800

    add Conclusion  class

--- models/Results.js
+++ models/Results.js
@@ -1,15 +1,20 @@
 /**
  * Created by joy on 2015/6/14.
  */
-var results=[];
 
-var root={
-    category:4,
-    title: "眼球充血",
-    children:[
-
-    ]
+/**
+ * class Diagnosis
+ * instance field: title, advice, emergency rating.
+ * @constructor
+ */
+function Conclusion(obj){
+    this.title=obj.titie;
+    this.advice=obj.advice;
+    this.rating=obj.rating;
 }
+
+var Conclusion=[];
+
 
 
 
