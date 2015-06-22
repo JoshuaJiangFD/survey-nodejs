45d6a2be3c25ab8d4eab214d923a25cbad95b369
JoshuaJiangFD <776456006@qq.com>
2015-06-22 20:30:11 +0800

    add tasks for production deployment. like clean, javascripts, stylesheet.

--- gulpfile.js
+++ gulpfile.js
@@ -9,14 +9,22 @@ var reactify = require('reactify');//convert jsx file to js file
 var source = require('vinyl-source-stream');
 var streamify=require('gulp-streamify');
 var watchify = require('watchify');//watch the file update and automatically trigger the build task
+var concat=require('gulp-concat');
+var rename=require('gulp-rename');
+var del=require('del');
+var minifycss=require('gulp-minify-css');
 
 
 var path = {
     HTML: 'views/index.ejs',
-    MINIFIED_OUT: 'app.min.js',
-    OUT:'app.js',
+    MINIFIED_OUT_APPJS:'app.min.js',
+    MINIFIED_OUT_CSS:'all.min.css',
+    OUT_APPJS:'app.js',
     ENTRY_POINT: './public/javascripts/src/app.js',
-    DEST_SRC:'./public/javascripts/build/'
+    DEST_SRC:'./public/javascripts/build/',
+    DEST_BUILD_JS:'dist/build/javascripts/',
+    DEST_BUILD_CSS:'dist/build/stylesheets/',
+    DEST_BUILD_VIEW:'dist/build/views/'
 };
 
 gulp.task('watch',function(){
@@ -31,7 +39,7 @@ gulp.task('watch',function(){
 
     return watcher.on('update',function(){
         watcher.bundle()
-            .pipe(source(path.OUT))
+            .pipe(source(path.OUT_APPJS))
             .pipe(gulp.dest(path.DEST_SRC));
         console.log("updated from watcher");
     }).bundle()
@@ -39,40 +47,67 @@ gulp.task('watch',function(){
         .pipe(gulp.dest(path.DEST_SRC));
 });
 
+/**
+ * browserify all the reat js files into app.min.js(variable path.MINIFIED_OUT)
+ * and uglify it then copy into dist/build folder(variable path.MINIFIED_OUT)
+ */
+gulp.task('javascripts', function(){
+   /*1. build app.min.js*/
+   browserify({
+       entries: [path.ENTRY_POINT],
+       transform: [reactify]
+   }).bundle()
+    .pipe(source(path.MINIFIED_OUT_APPJS))
+    .pipe(streamify(uglify(path.MINIFIED_OUT_APPJS)))
+    .pipe(gulp.dest(path.DEST_BUILD_JS));
+});
+
+/**
+ * build all.min.css, let is depends on 'javascripts' task
+ */
+gulp.task('stylesheets',['javascripts'],function(){
+    //include all the files under public/stylesheets but exclude reserved.css file with '!' prefixed.
+    gulp.src(['./public/stylesheets/*.css', '!./public/stylesheets/reserved.css'])
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest(path.DEST_BUILD_CSS))
+    .pipe(rename({suffix: '.min'}))
+   // .pipe(rename(path.MINIFIED_OUT_CSS))
+    .pipe(streamify(minifycss(path.MINIFIED_OUT_CSS)))
+    .pipe(gulp.dest(path.DEST_BUILD_CSS));
+});
+
+/**
+ * replace   <!--build:css--> in index.ejs file
+ */
+gulp.task('replaceHTML', function(){
+    gulp.src(path.HTML)
+    .pipe(htmlreplace({
+            'css': path.DEST_BUILD_CSS+path.MINIFIED_OUT_CSS
+        }))
+    .pipe(gulp.dest(path.DEST_BUILD_VIEW));
+});
+
+
+/**
+ * clean task for deleting all files in /dist folder
+ */
+gulp.task('clean', function(cb){
+   //return gulp.src(['/dist/build'],{read:false})
+   // .pipe(clean({force:true}));
+    del([
+        path.DEST_BUILD_JS,
+        path.DEST_BUILD_CSS,
+        path.DEST_BUILD_VIEW
+    ],cb);
+});
+
+
+/**
+ * run when development
+ */
 gulp.task('default', ['watch']);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//gulp.task('js', function(){
-//    browserify('./public/javascripts/src/app.js')
-//        .transform(reactify)
-//        .bundle()
-//        .pipe(source('app.js'))
-//        .pipe(gulp.dest('public/javascripts/build/'));
-//});
-
+/**
+ * run before deployment
+ */
+gulp.task('production', ['javascripts','stylesheets','replaceHTML']);
